2c7ab1dee3058e72322496af51fe9282ebc1d63f
Alexei Rusu <rusualexei1995@gmail.com>
2023-09-18 21:30:15 -0400

    bio change

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -26,19 +26,21 @@ function Bio(props){
 
 
           
-                <p>About me:</p>
+                {/* <p>About me:</p> */}
                 {/* <p>my name is Alexei, and I am a <span id="accent" style={{fontWeight: "bolder", textTransform:"uppercase"}}>full-stack web developer</span> with a background in choreography and a <span style={{fontWeight: "bolder"}}>unique perspective</span> to the development process. My combination of technical expertise and <span style={{fontWeight: "bolder"}}>creative vision</span>, honed by my choreographic education, allows me to approach problem-solving and design from a holistic perspective.  I am a team player, looking to integrate technical and artistic elements to create intuitive and visually engaging user experiences that inspire and connect with users. <span style={{color:"lightgreen"}}>Always eager to learn and grow</span> in the exciting field of development.</p>  */}
                 {/* fgfgf */}
                 <p className={toggle ? "falseStyle" : "trueStyle"}>
-                    my name is Alexei, and I am a <span id="accent" style={{}}>FULL-STACK SOFTWARE ENGINEER.</span> 
-                Through my experience as a software engineer, I have developed a strong foundation in full-stack web development with JavaScript technologies, including the MERN stack. I have also learned how to apply product development fundamentals, object-oriented programming, data modeling, and team collaboration strategies to create complete web applications with API systems. This portfolio showcases a range of individual and group projects that demonstrate my technical skills and creative vision. 
-                <br/>
-                My background in choreography has given me a unique perspective on problem-solving and design. I have a passion for integrating technical and artistic elements to create intuitive and visually engaging user experiences that inspire and connect with users. As a professional choreographer and dancer, I have also honed my communication, leadership, and adaptability skills, which are essential for working in dynamic and diverse teams. I am always eager to learn and grow in the exciting field of development, and I look forward to applying my competencies to new challenges and opportunities.</p> 
+                    My name is Alexei, and I am a <span id="accent" style={{}}>FULL-STACK SOFTWARE ENGINEER.</span> 
+                Through my experience as a software engineer, I have developed a strong foundation in full-stack web development with JavaScript technologies, including the MERN stack. I have also learned how to apply product development fundamentals, object-oriented programming, data modeling, and team collaboration strategies to create complete web applications with API systems. 
+                <br/><br/>
+                This portfolio showcases a range of individual and group projects that demonstrate my technical skills and creative vision. 
+                <br/><br/>
+                My background in choreography has given me a unique perspective on problem-solving and design. I have a passion for integrating technical and artistic elements to create intuitive and visually engaging user experiences that inspire and connect with users. As a professional choreographer and dancer, I have also honed my communication, leadership, and adaptability skills, which are essential for working in dynamic and diverse teams. I am always eager to learn and grow in the exciting field of development.</p> 
                 <span className='clampButton' onClick={handleClick}>{toggle ? "close" : "read more"}</span>
                 <div className='cred-container'>
                     <a href="mailto:rusualexei1995@gmail.com">&#9993; rusualexei1995@gmail.com</a>
                     <a  href={certificate} target="_blank" > Certification</a>        
-                      <a className="download-button" href={pdf} download>&#8681; RESUME</a>           
+                    <a className="download-button" href={pdf} download>&#8681; RESUME</a>           
                 </div>
 
                 
