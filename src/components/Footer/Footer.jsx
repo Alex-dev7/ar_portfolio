ce832a25227ba19165f7985a0717e4a88b6e32e9
Alex-dev7 <rusualexei1995@gmail.com>
2025-04-03 12:33:31 -0400

    context bug fix on show page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import './footer.scss'
 import ContactForm from '../Contact/ContactForm'
-import { ToggleContext } from '../../context/ToggleContext'
-import { useContext } from 'react';
+import { ToggleContext } from '../../context/ToggleContext';
+import React, { useContext } from 'react';
 
 function Footer(props){
     const { toggle, changeToggle } = useContext(ToggleContext);
@@ -18,7 +18,7 @@ function Footer(props){
             className="p-container" id='contact'>
                 <p>Developing a project?</p>
                 <p id="underline">
-                    <a href="#contact" id="contact-link" onClick={changeToggle}>Let's connect.</a>
+                    {/* <a href="#contact" id="contact-link" onClick={changeToggle}>Let's connect.</a> */}
                 </p>
             </div>
             <div className="links-container">
@@ -26,7 +26,7 @@ function Footer(props){
                     <li><a href="https://www.linkedin.com/in/alexei-rusu-dev/" target="_blank">linkedin</a></li>
                     <li><a href="https://github.com/Alex-dev7" target="_blank">github</a></li>
                     <li><a href="https://docs.google.com/document/d/1_u7fTIbalKg1xV9sb57bnsyPFXR-3FQb1tBLGiKst4E/edit?usp=sharing" >resume</a></li>
-                    <li><a href="#contact" onClick={changeToggle}>contact me</a></li>
+                    {/* <li><a href="#contact" onClick={changeToggle}>contact me</a></li> */}
                 </ul>
                 <ul className="copyright-wrapper">
                     <li className='copy-text'>© 2024 <span>Alexei Rusu</span></li>
