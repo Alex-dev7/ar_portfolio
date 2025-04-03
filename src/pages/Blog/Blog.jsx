5af09314515d321b785e641a2e82be95e5c3e51f
Alex-dev7 <rusualexei1995@gmail.com>
2025-04-03 16:50:53 -0400

    image path fix

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,22 +1,10 @@
-import blogs from "../../json/blogs.json"
 import './blog.scss'
-import {useState, useEffect} from "react"
-import { motion } from 'framer-motion';
 import { Link } from "react-router-dom";
 import TerminalWindow from "./components/TerminalWindow";
 
 
 function Blog(props) {
-  // const [mousePosition, setMousePosition] = useState({
-  //   x: 0,
-  //   y: 0
-  // });
-  // const [cursorVariant, setCursorVariant] = useState("default");
-  // const [classN, setClassN] = useState("cursor")
 
-
-  // const textEnter = () => setClassN("pointer")
-  // const textLeave = () => setClassN("cursor")
   return (
     <section className="blog-container">
       <Link id="home-button" to={`/`} >
