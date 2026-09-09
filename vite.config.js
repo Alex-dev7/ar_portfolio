287aaf2451e229396e0bc5d41941a4b7ed500b28
Alex-dev7 <rusualexei1995@gmail.com>
2026-09-09 11:09:47 -0400

    Deprecated legacy JS API

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  }
 })
+
+// export default defineConfig({
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         api: 'modern-compiler' // or "modern"
+//       }
+//     }
+//   }
+// })
